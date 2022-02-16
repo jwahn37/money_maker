c9e8d3a667e5423529cb3a3354b7477be7712163
JinwooAhn <jwahn37@gmail.com>
2022-02-16 15:12:27 +0900

    add example excel

--- stock_manager/cmd/main.py
+++ stock_manager/cmd/main.py
@@ -2,8 +2,12 @@ from excel import excel
 from stock import stock
 
 if __name__ == "__main__":
-    filePath = "/Users/jwahn37/Dropbox/제테크/돈관리_2022.xlsx"
-    sheetName = "투자2월"
+    filePath = "example.xlsx"
+    sheetName = "Sheet1"
+    dollarCell = 'D2'
+    nameRange = 'A2:A40'
+    priceRange = 'B2:B40'
+
     excel = excel.Excel(filePath=filePath, sheetName=sheetName, stock=stock.Stock())
-    excel.getStockPrice(nameRange = 'D2:D40', priceRange = 'F2:F40')
-    excel.getUSD('M84')
+    excel.getStockPrice(nameRange = nameRange, priceRange = priceRange)
+    excel.getUSD(dollarCell)
